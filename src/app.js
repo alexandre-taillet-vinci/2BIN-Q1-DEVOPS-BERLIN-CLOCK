48602b432f172eff4c4f83730b286d626e519431
WhyRoX <enes.12.yumer@gmail.com>
2024-11-10 20:42:55 +0100

    refactor: replace Main class with BerlinClock and add single minutes tests

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-export const OFF = 'O';
-export class Main {
+const OFF = 'O';
+const YELLOW = 'Y';
+export class BerlinClock {
     constructor() {
         this.lamps = {
             singleMinutes: [OFF, OFF, OFF, OFF]
@@ -8,5 +9,20 @@ export class Main {
 
     getSingleMinutes() {
         return this.lamps.singleMinutes.join('');
+    }
+
+    generateSingleMinutes() {
+        const minutes = this.date.getMinutes();
+        for (let i = 0; i < 4; i++) {
+            this.lamps.singleMinutes[i] = (minutes % 5) > i ? YELLOW : OFF;
+        }
+    }
+
+    setTime(unixTimestamp) {
+        this.date = new Date(unixTimestamp * 1000);
+    }
+
+    setTime(date) {
+        this.date = date;
     }
 }
