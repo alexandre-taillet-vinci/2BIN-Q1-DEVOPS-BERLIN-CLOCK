74a54de6cda1661f70cd4508d5f0ef18e750cdc7
alexandre-taillet-vinci <165775375+alexandre-taillet-vinci@users.noreply.github.com>
2024-11-10 21:20:09 +0100

    feature: add generateFiveMinutes method and corresponding tests

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const OFF = 'O';
 const YELLOW = 'Y';
+const RED = 'R';
 export class BerlinClock {
     constructor() {
         this.lamps = {
-            singleMinutes: [OFF, OFF, OFF, OFF]
+            singleMinutes: new Array(4).fill(OFF),
+            fiveMinutes: new Array(11).fill(OFF)
         };
     }
 
@@ -11,10 +13,28 @@ export class BerlinClock {
         return this.lamps.singleMinutes.join('');
     }
 
+
     generateSingleMinutes() {
         const minutes = this.date.getMinutes();
         for (let i = 0; i < 4; i++) {
             this.lamps.singleMinutes[i] = (minutes % 5) > i ? YELLOW : OFF;
+        }
+    }
+
+    getFiveMinutes(){
+        return this.lamps.fiveMinutes.join('');
+    }
+
+   
+
+    generateFiveMinutes() {
+        const minutes = this.date.getMinutes();
+        for (let i = 0; i < 11; i++) {
+            if ((i + 1) % 3 === 0 && (minutes / 5) > i) {
+                this.lamps.fiveMinutes[i] = RED;
+            } else {
+                this.lamps.fiveMinutes[i] = (minutes / 5) > i ? YELLOW : OFF;
+            }
         }
     }
 
