5987bf1da5023112885d9f9058d6bf030346cccb
WhyRoX <enes.12.yumer@gmail.com>
2024-11-10 20:29:28 +0100

    setup: class of berlin clock with default value

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,1 +1,10 @@
 import { Main } from '../src/app.js';
+
+describe("A suite of tests for the function setSingleMinutes", function() {
+    it('should return default value of the clock', () => {
+        let main = new Main();
+        console.log(main.getSingleMinutes());
+        expect(main.getSingleMinutes()).toBe('OOOO');
+    });
+    
+})
--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
+export const OFF = 'O';
 export class Main {
-    
+    constructor() {
+        this.lamps = {
+            singleMinutes: [OFF, OFF, OFF, OFF]
+        };
+    }
+
+    getSingleMinutes() {
+        return this.lamps.singleMinutes.join('');
+    }
 }
