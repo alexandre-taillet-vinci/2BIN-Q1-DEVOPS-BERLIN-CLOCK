d7ee16355dda8213035893e0dc7222c420a1385f
alexandre-taillet-vinci <165775375+alexandre-taillet-vinci@users.noreply.github.com>
2024-11-10 21:17:02 +0100

    Revert "feature: add generateFiveMinutes method and corresponding tests"
    This reverts commit bf916e7588c4d703a16d3e12fe99e96e96786d8f.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 const OFF = 'O';
 const YELLOW = 'Y';
-const RED = 'R';
 export class BerlinClock {
     constructor() {
         this.lamps = {
-            singleMinutes: new Array(4).fill(OFF),
-            fiveMinutes: new Array(11).fill(OFF)
+            singleMinutes: [OFF, OFF, OFF, OFF]
         };
     }
 
@@ -13,32 +11,12 @@ export class BerlinClock {
         return this.lamps.singleMinutes.join('');
     }
 
-
     generateSingleMinutes() {
         const minutes = this.date.getMinutes();
         for (let i = 0; i < 4; i++) {
             this.lamps.singleMinutes[i] = (minutes % 5) > i ? YELLOW : OFF;
         }
     }
-
-    getFiveMinutes(){
-        return this.lamps.fiveMinutes.join('');
-    }
-
-   
-
-    generateFiveMinutes() {
-        const minutes = this.date.getMinutes();
-        for (let i = 0; i < 11; i++) {
-            if ((i + 1) % 3 === 0 && (minutes / 5) > i) {
-                this.lamps.fiveMinutes[i] = RED;
-            } else {
-                this.lamps.fiveMinutes[i] = (minutes / 5) > i ? YELLOW : OFF;
-            }
-        }
-    }
-
-
 
     setTime(unixTimestamp) {
         this.date = new Date(unixTimestamp * 1000);
